ce58d5c32c4654466fef2d1f652026ba142dafc6
2eg4rizve <2eg4rizve.com>
2023-09-11 20:04:21 +0600

    all btn

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -6,9 +6,16 @@ import Cart from "../Cart/Cart";
 const Bottles = () => {
 
     const [bottles, setBottles] = useState([]);
+    const [allBottles, setAllBottles] = useState([]);
     const [carts, setCarts] = useState([]);
     const [sum, setSum] = useState(0);
 
+    useEffect(() => {
+        fetch('bottles.json')
+            .then(res => res.json())
+            .then(data => setAllBottles(data))
+
+    }, [])
     useEffect(() => {
         fetch('bottles.json')
             .then(res => res.json())
@@ -28,24 +35,50 @@ const Bottles = () => {
         setSum(newSum);
     }
 
+    const all = () => {
+        
+        setBottles(allBottles);
+    }
+
     const increasingOrder = () => {
         console.log("increasing Order");
 
         const sortedProducts = [...bottles];
         sortedProducts.sort((a, b) => {
-           return  a.price - b.price
+            return a.price - b.price
         });
         setBottles(sortedProducts);
 
 
     }
 
-    const decreasingOrder =()=>{
-        const sortedProducts =[...bottles];
-        sortedProducts.sort((a,b) => {
-            return b.price-a.price;
+    const decreasingOrder = () => {
+        const sortedProducts = [...bottles];
+        sortedProducts.sort((a, b) => {
+            return b.price - a.price;
         });
         setBottles(sortedProducts);
+    }
+
+    const smallerThen40 = () => {
+
+        const result = allBottles.filter((item) => {
+            if (item.price <= 40) {
+                return item;
+            }
+        });
+        setBottles(result);
+
+    }
+
+    const biggerThen40 = () => {
+
+        const result = allBottles.filter((item) => {
+            if (item.price >= 40) {
+                return item;
+            }
+        })
+        setBottles(result);
     }
 
     return (
@@ -76,12 +109,25 @@ const Bottles = () => {
             </div>
             <p className="text-[30px] text-center font-bold text-blue-600">Bottles</p>
 
+            <div className="flex gap-4 my-[30px]">
 
-            <button onClick={increasingOrder} className="btn btn-primary">sort increasing order</button>
-            <br />
+                <button onClick={all} className="btn btn-primary">ALL</button>
+                <br />
 
-            <button onClick={decreasingOrder} className="btn btn-primary">sort decreasing order</button>
-            <br />
+                <button onClick={increasingOrder} className="btn btn-primary">Sort By Increasing Order</button>
+                <br />
+
+                <button onClick={decreasingOrder} className="btn btn-primary">Sort By Decreasing Order</button>
+                <br />
+
+                <button onClick={smallerThen40} className="btn btn-primary">Smaller Then 40$</button>
+                <br />
+
+                <button onClick={biggerThen40} className="btn btn-primary">Bigger Then 40$</button>
+                <br />
+
+            </div>
+
 
             <div className="grid grid-cols-5 gap-3">
                 {
